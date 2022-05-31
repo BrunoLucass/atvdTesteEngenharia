59f683a9efcd0aafd03844942284d34f85917e5c
Bruno Lucas <70291159+BrunoLucass@users.noreply.github.com>
2022-05-31 12:57:37 -0300

    projeto final

---
+++ src/test/java/teste.java
@@ -0,0 +1,86 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
+
+public class teste {
+
+    @Test
+    public void testabrirPagina () {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Pichau\\Desktop\\Eclipse\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+        driver.get("https://www.casasbahia.com.br/");
+
+        // teste para ver se irá abrir a página certa
+        String url = driver.getCurrentUrl();
+        assertEquals("https://www.casasbahia.com.br/", url);
+        // fecha a aplicação
+        driver.quit();
+
+    }
+    @Test
+    public void testClear() {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Pichau\\Desktop\\Eclipse\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get("https://www.casasbahia.com.br/");
+
+        // vai achar a caixa de busca, escrever playstation, apagar e escrever xbox
+        WebElement caixaDeBusca = driver.findElement(By.id("strBusca"));
+        caixaDeBusca.sendKeys("Playstation");
+        caixaDeBusca.clear();
+        caixaDeBusca.sendKeys("Xbox");
+
+
+        // vai verificar se entrou na página certa
+        String url = driver.getCurrentUrl();
+        assertEquals("https://www.casasbahia.com.br/", url);
+
+        // fecha a aplicação
+        driver.quit();
+
+    }
+    @Test
+    public void testSearch() {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Pichau\\Desktop\\Eclipse\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.casasbahia.com.br/");
+
+        // vai achar a caixa de busca e escrever Panelas
+        WebElement caixaDeBusca = driver.findElement(By.id("strBusca"));
+        caixaDeBusca.sendKeys("Panelas");
+
+        // vai enviar clicando no botão
+        WebElement botaoBusca = driver.findElement(By.id("btnOK"));
+        botaoBusca.click();
+
+        // vai verificar se entrou na página certa
+        String url = driver.getCurrentUrl();
+        assertEquals("https://www.casasbahia.com.br/", url);
+
+        // fecha a aplicação
+        driver.quit();
+
+    }
+
+
+}
